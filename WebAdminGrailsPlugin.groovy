ceb847ac35920187cae7c450511d8f25a933689f
woxiaobao <574727356@qq.com>
2016-04-12 14:58:31 +0800

    他

--- WebAdminGrailsPlugin.groovy
+++ WebAdminGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class WebAdminGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.0.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
